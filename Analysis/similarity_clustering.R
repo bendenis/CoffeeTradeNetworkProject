3fa430d53f331d7beabd57eba46c881a7276ba6f
Ben <bendenisshaffer@gmail.com>
2017-11-21 19:19:13 -0500

    clustering anal

---
+++ Analysis/similarity_clustering.R
@@ -0,0 +1,35 @@
+library(igraph)
+library(stringr)
+library(purrr)
+library(GGally)
+library(dplyr)
+
+coffee_trade_2010 = read.csv("Data/Decorated/coffeeG10measures.csv")
+coffeeG10 = read_graph("Data/iGraphs/coffeeG10.gml", format = "gml")
+
+
+
+sim_ij = function(G, i, j){
+        
+        set1 = neighbors(G, v =  V(G)[i], mode = "all")
+        set2 = neighbors(G, v =  V(G)[j], mode = "all")
+        inter_set = intersect(set1, set2)
+        sim = length(inter_set)/(length(set1)+length(set2))
+        return(sim)
+}
+
+simV = vector(mode = "numeric", length = length(V(coffeeG10))^2)
+for(i in 1:207){
+        a = 207*(i-1) + 1
+        b = 207*i
+        sim_vals = unlist(lapply(1:207, function(j) sim_ij(coffeeG10, i, j)))
+        simV[a:b] = sim_vals
+}
+dim(simV) = c(207,207)
+heatmap(simV)
+
+
+
+simM = similarity(coffeeG10, vids = V(coffeeG10), mode = "all", method = "jaccard")
+
+
